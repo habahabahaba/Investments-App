6ba1fb5abe6f08e5fb3511ecdf493c2e2bf7af0d
Alex S <habahabahaba@outlook.com>
2025-01-23 05:36:15 +0400

    Rename project directory to 'Investments'. Use ResultsTable.jsx in Calculator.jsx, index.css: #result -> #results.

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -5,41 +5,26 @@ import { formatter } from '../util/investment';
 // Store:
 // React Router:
 // React:
-import { useReducer } from 'react';
 // Context:
 // Hooks:
 import useCalculator from '../hooks/useCalculator/useCalculator';
 // Components:
 import InputForm from './InputForm';
 import ResultsTable from './ResultsTable';
-import Table from './Table';
 
 function Calculator() {
   // State:
   const { inputState, inputDispatch, investmentResults, error } =
     useCalculator();
   console.log('[Calculator] reducer inputState: ', inputState);
+
   // JSX:
   const table =
+    // If all inputs are filled
     Object.values(inputState).every((value) => value !== '') &&
+    // If there are no errors
     Object.values(error).every((value) => !value) ? (
-      <Table
-        columnLabels={[
-          'Year',
-          'Investment Value',
-          'Interest (Year)',
-          'Total Interest',
-          'Invested Capital',
-        ]}
-        renderFns={[
-          (y) => {
-            return y;
-          },
-        ].concat(new Array(4).fill((data) => formatter.format(data)))}
-        data={investmentResults}
-        className=''
-        id='result'
-      />
+      <ResultsTable investmentResults={investmentResults} />
     ) : null;
 
   return (
@@ -49,7 +34,6 @@ function Calculator() {
         inputDispatch={inputDispatch}
         error={error}
       />
-      {/* <ResultsTable results={investmentResults} /> */}
       {table}
     </>
   );
--- src/Components/ResultsTable.jsx
+++ src/Components/ResultsTable.jsx
@@ -1,3 +1,5 @@
+// Utils:
+import { formatter } from '../util/investment';
 // 3rd party:
 // Redux RTK:
 // Store:
@@ -6,11 +8,37 @@
 // Context:
 // Hooks:
 // Components:
+import Table from './Table';
 // CSS:
 
-function ResultsTable({ results }) {
+function ResultsTable({ investmentResults }) {
+  // Render functions:
+  const renderFns = [
+    // Do not format year number
+    (y) => {
+      return y;
+    },
+  ].concat(
+    // Currency formatters
+    new Array(4).fill((data) => formatter.format(data))
+  );
+
   // JSX:
-  return <p>{JSON.stringify(results, null, 2)}</p>;
+  return (
+    <Table
+      columnLabels={[
+        'Year',
+        'Investment Value',
+        'Interest (Year)',
+        'Total Interest',
+        'Invested Capital',
+      ]}
+      renderFns={renderFns}
+      data={investmentResults}
+      className=''
+      id='results'
+    />
+  );
 }
 
 export default ResultsTable;
